69f364e719dc5d8da6809d5e6baa297a9862ded3
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2016-04-17 20:59:04 -0400

    Import ext. types from sxn module as one level up

--- pysaxon/__init__.py
+++ pysaxon/__init__.py
@@ -2,7 +2,7 @@ from .version import version as __version__
 
 # Delay importing extension modules till after they are built...
 try:
-    from . import sxn
+    from .sxn import *
     from . import xdm
 except ImportError:
     pass
